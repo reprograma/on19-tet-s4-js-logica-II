92378f8ad91f93246f06241636365ec06194fcdb
Giuliana Zambotto Furlan <giuliana.furlan@ambevtech.com.br>
2022-09-05 17:49:59 -0300

    extras

---
+++ exercicios/extra/foreach.js
@@ -0,0 +1,28 @@
+const arrNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// Usando foreach multiplique cada item por 2 salvando numa nova array, imprima o resultado
+
+const arrObjetos = [
+  {
+    nome: "Maria",
+    idade: 21
+  },
+  {
+    nome: "Josefa",
+    idade: 12
+  },
+  {
+    nome: "Clementina",
+    idade: 55
+  },
+  {
+    nome: "Gustava",
+    idade: 14
+  },
+  {
+    nome: "Chablana",
+    idade: 33
+  }
+];
+
+//usando foreach adicione 1 a todas as idades
