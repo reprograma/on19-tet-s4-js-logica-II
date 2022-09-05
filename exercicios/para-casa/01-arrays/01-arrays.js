ed375c13b3437bc4a8781e974ca72ad4173853e2
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-09-05 20:39:47 -0300

    02 exercicio

--- exercicios/para-casa/01-arrays/01-arrays.js
+++ exercicios/para-casa/01-arrays/01-arrays.js
@@ -5,15 +5,16 @@
 const arrayExercicio = ["Banana", "Pera", "Uva", "Maça", "Figo", "Abacaxi", "Caviar"];
 
 // Imprima o segundo item da array
-
+console.log("O segundo item é", arrayExercicio[1]);
 
 // Imprima o último item da array
-
+console.log("O ultimo item é", arrayExercicio[6]);
 
 // Imprima o item do meio da array
-
+console.log("O item do meio é", arrayExercicio[3]);
 
 // Imprima "Eu gosto mais de banana do que figo" usando a array no lugar das frutas
 
+console.log("Eu gosto mais de", listaArrays[0][0], listaArrays[1][0]);
 
 // Imprima o item que não é uma fruta
---
+++ exercicios/para-sala/03-loops/01-loops.js
@@ -0,0 +1,43 @@
+console.clear();
+const listaFrutas = [
+  "Banana",
+  "Manga",
+  "Maça",
+  "Pera",
+  "Abacaxi",
+  "Banana",
+  "Manga",
+  "Maça",
+  "Pera",
+  "Abacaxi",
+];
+
+let contador = 0;
+let frutasAchadas = 0;
+const palavraProcurada = "Abacaxi";
+
+//while
+while (contador < listaFrutas.length) {
+  contador++;
+  if (listaFrutas[contador] === palavraProcurada) {
+    frutasAchadas++
+  }
+}
+
+console.log("While: Achei " + frutasAchadas + " dessa fruta");
+frutasAchadas = 0;
+
+//for de C
+for (let index = 0; index < listaFrutas.length; index++) {
+  listaFrutas[index] = listaFrutas[index][0].toLowerCase() + listaFrutas[index].slice(1);
+  console.log(
+    index,
+    listaFrutas[index],
+    listaFrutas[index] === palavraProcurada
+  );
+  if (listaFrutas[index] === palavraProcurada) {
+    frutasAchadas++
+  }
+}
+
+console.log("For: Achei " + frutasAchadas + " dessa fruta");
