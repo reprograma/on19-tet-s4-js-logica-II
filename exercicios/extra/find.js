62b60feb7a60ec21157deaefd8354f66293b21ba
pâmela santos <pssantos201@gmail.com>
2022-09-06 18:50:51 -0300

    add extra exercise

--- exercicios/extra/find.js
+++ exercicios/extra/find.js
@@ -1,1 +1,19 @@
 // crie um exercício que use o find
+
+const meusPets = [
+    {
+        nome: 'Babete',
+        patas: 4,
+        olhos: 2,
+        adotada: true
+    },
+    {
+        nome: 'Brisa',
+        patas: 4,
+        olhos: 1,
+        adotada: true
+    }
+]
+
+console.log(meusPets.find(cach => cach.olhos < 2))  
+  // { nome: 'Brisa', patas: 4, olhos: 1, adotada: true }
