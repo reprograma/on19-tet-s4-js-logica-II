9c21f8e4dc38af5606cb10cd75ec294e38178a0e
Karen Ferraz <karen.ferrazribeiro@hotmail.com>
2022-09-05 22:00:30 -0300

    modificações

---
+++ exercicios/para-sala/01-arrays/01-arrays.js
@@ -0,0 +1,23 @@
+//declarando uma array 
+
+const listaNumeros = [1,2,3,4,5,6,7];
+
+const listaTextos = ["Banana", "Pera", "Maça"];
+
+const listaMista = [55, "olá", true, undefined];
+
+const listaObjetos = [{ nome: "Giu", }, {idade: 34}];
+
+//Acessando itens da array 
+
+console.log(listaNumeros[3]);
+
+console.log(listaObjetos[0].nome);
+
+const temperos = [
+    ["Macarrao", "Sushi"],
+    ["Salsinha", "Sal"]
+];
+//para imprimir o item "sal"
+console.log(temperos[1][1]);
+
