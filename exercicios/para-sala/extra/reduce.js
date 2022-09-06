6d30967bbfe21f1e0ac34503091371bc4789abc2
elisangelamanuel <elisangelasofs@gmail.com>
2022-09-05 22:07:19 -0300

    Update exercises

--- exercicios/para-sala/extra/reduce.js
+++ exercicios/para-sala/extra/reduce.js
@@ -1,4 +1,7 @@
+console.clear();
 const arrNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const somado = arrNumeros.reduce((anterior, atual) => anterior += atual * 2);
 
 const arrObjetos = [
   {
@@ -22,3 +25,7 @@ const arrObjetos = [
     idade: 33
   }
 ];
+
+const somaObj = arrObjetos.reduce((anterior, atual) => anterior += atual.idade, 0)
+
+console.log(somaObj);
