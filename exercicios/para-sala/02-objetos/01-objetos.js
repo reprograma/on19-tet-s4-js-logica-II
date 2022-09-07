0e2c2515a71ca6dac56d6ecb22f7640281844b0e
Larissa Fontes <larissa.fontes21@gmail.com>
2022-09-07 16:59:13 -0300

    update

---
+++ exercicios/para-sala/02-objetos/01-objetos.js
@@ -0,0 +1,42 @@
+console.clear();
+// Objetos
+
+const pessoa = {
+  nome: "Giu",
+  idade: 42,
+  temCNH: true,
+  hobbies: ["Séries", "Games"],
+  carro: {
+    marca: "fiat",
+    nome: "Palio",
+    motor: {
+      potencia: 100,
+      combustivel: "gasolina"
+    }
+  },
+  apresentar: function () {
+    console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos. E meu primeiro hobby é ${this.hobbies[0]}`);
+  },
+  saudar: () => {
+    //console.log(this.); <== não tem this na arrow function
+  }
+}
+
+pessoa.apresentar();
+
+console.log("Nome: ", pessoa.nome);
+
+pessoa.hobbies.push("Codar");
+console.log("Hobby: ", pessoa.hobbies);
+
+pessoa.carro.motor.potencia += 50;
+console.log(pessoa.carro.motor);
+
+pessoa.moto = {
+  potencia: 80,
+  combustivel: "Álcool"
+}
+
+console.log(pessoa);
+
+pessoa.saudar()
---
+++ exercicios/para-sala/03-loops/04-loops.js
@@ -0,0 +1,35 @@
+console.clear();
+const usuarios = [
+  {
+    nome: "Fulana",
+    idade: 31,
+    cpf: 394875384645645
+  },
+  {
+    nome: "Fulana2",
+    idade: 43,
+    cpf: 394875384456
+  },
+  {
+    nome: "Fulana3",
+    idade: 22,
+    cpf: 34564565384675
+  },
+  {
+    nome: "Fulana4",
+    idade: 17,
+    cpf: 39445644675
+  }
+];
+
+const proFront = usuarios.map(
+  (pessoa) => {
+    const sanitizado = {
+      nome: pessoa.nome,
+      idade: pessoa.idade < 18 ? "Jovem" : "Adulto" // IF em códigos // condition ? expr1 : expr2 
+    };
+    return sanitizado;
+  }
+);
+
+console.log(usuarios, proFront);
