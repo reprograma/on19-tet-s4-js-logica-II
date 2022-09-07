f037eb14bf2d546c1c13fa0126f84e88313629c8
Shirlene Teles <shiteles@gmail.com>
2022-09-07 12:25:35 -0300

    Add resolução exercicios com filter,find,foreach e reduce

--- exercicios/extra/filter.js
+++ exercicios/extra/filter.js
@@ -1,8 +1,12 @@
 const arrNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 // Usando filter, imprima apenas os números acima de 10
+const acimaDez = arrNumeros.filter(numero => numero > 10);
+console.log(acimaDez)
 
 // Usando filter, imprima apenas os números pares
+const numpar = arrNumeros.filter(numero => numero % 2 === 0);
+console.log(numpar)
 
 const arrObjetos = [
   {
@@ -28,6 +32,11 @@ const arrObjetos = [
 ];
 
 // usando filter, imprima uma array só com os adultos
+const adultos = arrObjetos.filter(arrObjetos => arrObjetos.idade > 18);
+console.log(adultos)
 
 // usando filter, imprima apenas as pessoas cuja letra inicial do nome seja C
 
+
+const resultado = arrObjetos.filter( arrObjetos  => arrObjetos.nome[0] === "C");
+console.log(resultado);
--- exercicios/extra/reduce.js
+++ exercicios/extra/reduce.js
@@ -1,8 +1,18 @@
 const arrNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 // Usando reduce, Some todos os números da array
+const somando = arrNumeros.reduce((anterior, atual) => anterior += atual);
+console.log("Soma de todos os itens:", somando);
 
 // Some todas os número pares (pode usar filter em conjunto ou apenas o reduce)
+const somaPares = arrNumeros
+  .filter(item => item % 2 === 0)
+  .reduce((anterior, atual) => {
+    //console.log(anterior + "+" + atual);
+    return anterior + atual;
+  }, 0);
+
+console.log("Soma de todos os itens pares:", somaPares);
 
 const arrObjetos = [
   {
@@ -28,5 +38,14 @@ const arrObjetos = [
 ];
 
 // Some todas as idades das pessoas do objeto acima
+const somaIdades = arrObjetos.reduce((anterior, atual) => anterior += atual.idade,0)
+
+console.log("Soma de todas as idades:", somaIdades);
 
 // Some todas as idades das pessoas menores de idade
+
+const somaMenores = arrObjetos.filter((menor) => menor.idade < 18)
+.map((arrObjetos) => arrObjetos.idade,0)
+.reduce((anterior, atual) => anterior += atual)
+console.log("Soma dos menores de idade:", somaMenores)
+
