ca99296b59cba8d577b8c003eae2110d1c76177b
Dafilyn Mariano <dafilyn_mariano@hotmail.com>
2022-09-08 17:37:05 -0300

    resoluções das atividades

--- exercicios/para-casa/01-arrays/09-arrays.js
+++ exercicios/para-casa/01-arrays/09-arrays.js
@@ -14,17 +14,7 @@ const arrayExercicio = [
 console.log(arrayExercicio.reverse())
 
 // coloque a array em ordem alfabética
-function comparar(itemEsquerdo, itemDireito) {
-  if (itemDireito > itemEsquerdo) {
-    return -1;
-  }
-  if (itemEsquerdo > itemDireito) {
-    return 1;
-  }
-  return 0;
-}
-
-const ordemNaLista = arrayExercicio.sort(comparar);
-console.log(arrayExercicio);
+console.log(arrayExercicio.sort())
 
 // coloque a array em ordem alfabética decrescente z -> a
+console.log(arrayExercicio.sort().reverse())
--- exercicios/para-casa/01-arrays/11-arrays.js
+++ exercicios/para-casa/01-arrays/11-arrays.js
@@ -11,11 +11,20 @@ const arrayExercicio = [
 ];
 
 // adicione na array uma fruta usando push
+arrayExercicio.push("morango")
+console.log(arrayExercicio)
 
 // remova o último item da array usando pop
+arrayExercicio.pop()
+console.log(arrayExercicio)
 
 // adicione uma fruta ao início da array usando unshift
+arrayExercicio.unshift("amora")
+console.log(arrayExercicio)
 
 // remova o primeiro item da array usando shift
+arrayExercicio.shift()
+console.log(arrayExercicio)
 
 // a array resultante deverá ser igual a inicial
+console.log(arrayExercicio)
