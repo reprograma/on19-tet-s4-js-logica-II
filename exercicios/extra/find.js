3a7dc32b8e28bac816db8c5cb7660ec15c78bce6
Mikaela Clotilde <mikaelacs29@gmail.com>
2022-09-08 15:37:15 -0300

    Novas atualizações - Respostas extras

--- exercicios/extra/find.js
+++ exercicios/extra/find.js
@@ -1,1 +1,24 @@
 // crie um execício que use o find
+
+const repteisDePernambuco = [
+    {
+        nome: 'Mabuya',
+        patas: 4,
+        olhos: 2,
+        pernambuco: true
+    },
+    {
+        nome: 'surucucu',
+        patas: 0,
+        olhos: 2,
+        pernambuco: true
+    },
+    {
+        nome: 'ophiodes',
+        patas: 2,
+        olhos: 2,
+        pernambuco: true
+    }
+]
+
+console.log(repteisDePernambuco.find(cach => cach.patas > 2))  
--- exercicios/extra/foreach.js
+++ exercicios/extra/foreach.js
@@ -1,6 +1,9 @@
 const arrNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 // Usando foreach multiplique cada item por 2 salvando numa nova array, imprima o resultado
+let  arrNúmerosMultiplicado  =  [ ]
+const  multiplicaArrNúmeros  =  arrNumeros . forEach ( num  =>  arrNúmerosMultiplicado . push ( num  *  2 ) )
+console.log ( arrNúmerosMultiplicado )
 
 const arrObjetos = [
   {
@@ -26,3 +29,5 @@ const arrObjetos = [
 ];
 
 //usando foreach adicione 1 a todas as idades
+arrObjetos.forEach(pessoa => { 
+  console.log(`Somando 1 na idade de ${pessoa.nome} obtemos a idade: ${pessoa.idade + 1}`)})
