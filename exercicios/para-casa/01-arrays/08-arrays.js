6a42fbe0ee2cc5919e8a30593c0f794b9f3e94a2
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-09-07 22:55:36 -0300

    :sparkles: exercise about trim method

--- exercicios/para-casa/01-arrays/08-arrays.js
+++ exercicios/para-casa/01-arrays/08-arrays.js
@@ -1,5 +1,17 @@
-const dia = 1;
-const mes = 2;
-const ano = 2027;
+const dia = 1
+const mes = 2
+const ano = 2027
 
 // imprima 01/02/2027 usando as constantes acima e padStart para adicionar o zero em dia e mes
+
+//convertendo o number para string, pq o metodo padstart só funciona com strings e não com numbers
+
+let diaStringado = dia.toString()
+let mesStringado = mes.toString()
+
+let diaComPadStart = diaStringado.padStart(2, '0')
+
+let mesComPadStart = mesStringado.padStart(2, '0')
+
+let novoAno = `${diaComPadStart}/${mesComPadStart}/${ano}`
+console.log(novoAno)
