425a61b2fab82c3471f83cdfa6f819014ac0fb45
elisangelamanuel <elisangelasofs@gmail.com>
2022-09-09 19:51:27 -0300

    Update exercises

--- exercicios/para-casa/02-objetos/01-objetos.js
+++ exercicios/para-casa/02-objetos/01-objetos.js
@@ -7,15 +7,18 @@ const pessoa = {
 };
 
 // imprima o nome da pessoa
-
+console.log(pessoa.nome);
 
 // imprima de maneira amigável se a pessoa possui CNH
-
+msgConfirmacaoCnh = "Muito bom, você tem CNH" ; pessoa.temCNH == true;
+console.log(msgConfirmacaoCnh);
 
 // imprima o último apelido da pessoa
-
+console.log(pessoa.apelidos[2]);
 
 // Troque o nome da pessoa e imprima, usando somente o objeto
-
+console.log(pessoa.nome.replace("Fulana", "NomeTrocado"));
 
 // adicione uma noma propriedade chama gênero com o valor 'feminino' e imprima, usando somente o objeto
+let addGenero = pessoa["genero"] = "feminino";
+console.log(pessoa.genero);
--- exercicios/para-casa/02-objetos/04-objetos.js
+++ exercicios/para-casa/02-objetos/04-objetos.js
@@ -1,6 +1,6 @@
 const pessoa = {
   nome: "Fulana",
-  idade: 41,
+  idade: 77, // valor declarado e inicializado em 41
   altura: 1.74,
   temCNH: true,
   apelidos: ["Fu", "Ful", "Lana"]
@@ -12,6 +12,16 @@ const pessoa = {
 // sendo jovem até exatamente 18 anos, adulta até exatamente 64 e idosa acima ou igual 65 anos
 // ex: Olá, sou uma pessoa jovem!
 
-
+let addMetodo = pessoa["faixaEtaria"];
+addMetodo = pessoa["faixaEtaria"] = function() {
+  if (this.idade == 18 || this.idade < 18) {
+    console.log(`Olá, sou uma pessoa jovem`);
+} else if (this.idade == 64 || this.idade>18 && this.idade<65) {
+  console.log(`Olá, sou uma pessoa adulta`);
+} else if (this.idade >= 65) {
+  console.log(`Olá, sou uma pessoa idosa`);
+}};
+/*pessoa.faixaEtaria(); //executa o método faixaEtaria com valor original de pessoa.idade = 41 e imprime a frase: Olá, sou uma pessoa adulta*/
 
 // altere a idade da pessoa do objeto para 77 anos e utilize o método que criou para imprimir sua faixa etária
+pessoa.faixaEtaria(); // Após atualizar o valor de 41 para 77, conforme solicitado, executa o método faixaEtaria e imprime a frase: Olá, sou uma pessoa idosa
