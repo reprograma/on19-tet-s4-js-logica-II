e4e5a6495d77f456b107ef8619d27bdd95cca196
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-12-14 02:26:01 -0300

    adiciona exercícios de objetos

--- exercicios/para-casa/02-objetos/05-objetos.js
+++ exercicios/para-casa/02-objetos/05-objetos.js
@@ -1,9 +1,15 @@
 const pessoa = {
-  nome: "Fulana",
+  nome: 'Fulana',
   idade: 41,
   altura: 1.74,
   temCNH: true,
-  apelidos: ["Fu", "Ful", "Lana"]
-};
+  apelidos: ['Fu', 'Ful', 'Lana']
+}
 
 // usando for in, imprima todas as propriedades e seus valores, um por um.
+for (const k in pessoa) {
+  if (Object.hasOwnProperty.call(pessoa, k)) {
+    const element = pessoa[k]
+    console.log(k, element)
+  }
+}
