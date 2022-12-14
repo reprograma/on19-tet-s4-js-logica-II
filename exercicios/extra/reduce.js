5c3ad440a5537e4b85156cdf2a15339baca6693e
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-12-14 02:43:59 -0300

    adiciona exercícios extras

--- exercicios/extra/reduce.js
+++ exercicios/extra/reduce.js
@@ -1,32 +1,51 @@
-const arrNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const arrNumeros = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+]
 
-// Usando reduce, Some todos os números da array
+// Usando reduce, some todos os números da array
+const soma = arrNumeros.reduce((anterior, atual) => (anterior += atual))
+console.log(soma)
 
-// Some todas os número pares (pode usar filter em conjunto ou apenas o reduce)
+// Some todas os números pares (pode usar filter em conjunto ou apenas o reduce)
+const somaPares = arrNumeros.reduce((num1, num2) =>
+  num1 % 2 === 0 ? (num1 += num2) : (num1 += num2 % 2 === 0)
+)
+console.log(somaPares)
 
 const arrObjetos = [
   {
-    nome: "Maria",
+    nome: 'Maria',
     idade: 21
   },
   {
-    nome: "Josefa",
+    nome: 'Josefa',
     idade: 12
   },
   {
-    nome: "Clementina",
+    nome: 'Clementina',
     idade: 55
   },
   {
-    nome: "Gustava",
+    nome: 'Gustava',
     idade: 14
   },
   {
-    nome: "Chablana",
+    nome: 'Chablana',
     idade: 33
   }
-];
+]
 
 // Some todas as idades das pessoas do objeto acima
+const somaIdades = arrObjetos.reduce(
+  (iteradorIdade, idadeAtual) => (iteradorIdade += idadeAtual.idade),
+  0
+)
+console.log(somaIdades)
 
 // Some todas as idades das pessoas menores de idade
+const novoArray = arrObjetos.filter(pessoa => pessoa.idade < 18)
+const somaMenoresDeIdade = novoArray.reduce(
+  (iteradorIdade, idadeAtual, i) => (iteradorIdade += idadeAtual.idade),
+  0
+)
+console.log(somaMenoresDeIdade)
